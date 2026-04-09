3d4be18e6c246cdace48112f4a01ba09fd4ab3b7
Oktaviano <oktavianosahru.r@gmail.com>
2026-04-09 17:09:14 +0700

    feat: add generic error handling

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { signup, login } from "../controllers/authController.js";
+import { errorHandler } from "../error-handler.js";
 
 const authRoutes: Router = Router();
 
-authRoutes.post("/signup", signup);
-authRoutes.post("/login", login);
+authRoutes.post("/signup", errorHandler(signup));
+authRoutes.post("/login", errorHandler(login));
 
 export default authRoutes;
