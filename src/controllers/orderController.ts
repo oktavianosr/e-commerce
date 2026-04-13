58c22c04cfb6d646f419bf46ff26b52a40dce295
Oktaviano <oktavianosahru.r@gmail.com>
2026-04-13 18:22:13 +0700

    feat: placing order from user cart

---
+++ src/controllers/orderController.ts
@@ -0,0 +1,76 @@
+import { NotFoundException } from '../exceptions/not-found.js';
+import { prismaClient } from '../index.js';
+import type { AuthenticatedRequest } from '../types/authenticated-request.js';
+import { BaseController, type Response } from './base.controller.js';
+import { ErrorCode } from '../exceptions/root.js';
+
+class OrderController extends BaseController {
+    createOrder = async (req: AuthenticatedRequest, res: Response) => {
+        // list all the cart items and proceed if cart is not empty
+        const cartItems = await prismaClient.cartItem.findMany({
+            where: { userId: req.user.id },
+            include: { product: true },
+        });
+
+        if (cartItems.length === 0) {
+            return this.respondError(res, 'Cart is empty', 400);
+        }
+
+        // create a transaction
+        const order = await prismaClient.$transaction(async (tx) => {
+            // calculate the total amount
+            const totalAmount = cartItems.reduce((prev, current) => {
+                return prev + current.quantity * Number(current.product.price);
+            }, 0);
+
+            // fetch address of user
+            const address = await tx.address.findFirst({
+                where: { id: Number(req.user.defaultShippingAddress) },
+            });
+
+            if (!address) {
+                throw new NotFoundException(
+                    'Address not found',
+                    ErrorCode.ADDRESS_NOT_FOUND
+                );
+            }
+
+            // create order and order products
+            const order = await tx.order.create({
+                data: {
+                    userId: req.user.id,
+                    netAmount: totalAmount,
+                    address: address.formattedAddress,
+                    products: {
+                        create: cartItems.map((item) => ({
+                            productId: item.product.id,
+                            quantity: item.quantity,
+                        })),
+                    },
+                },
+            });
+
+            // create event
+            await tx.orderEvent.create({
+                data: { orderId: order.id },
+            });
+
+            // clear cart
+            await tx.cartItem.deleteMany({
+                where: { userId: req.user.id },
+            });
+
+            return order;
+        });
+
+        this.respondSuccess(res, order, 'Order created successfully', 201);
+    };
+
+    listOrders = async (req: AuthenticatedRequest, res: Response) => {};
+
+    cancelOrder = async (req: AuthenticatedRequest, res: Response) => {};
+
+    getOrderById = async (req: AuthenticatedRequest, res: Response) => {};
+}
+
+export default new OrderController();
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import authRoutes from './auth.js';
 import productsRoutes from './products.js';
 import usersRoutes from './users.js';
 import cartRoutes from './cart.js';
+import ordersRoutes from './orders.js';
 
 const rootRouter: Router = Router();
 
@@ -10,5 +11,6 @@ rootRouter.use('/auth', authRoutes);
 rootRouter.use('/products', productsRoutes);
 rootRouter.use('/users', usersRoutes);
 rootRouter.use('/cart', cartRoutes);
+rootRouter.use('/orders', ordersRoutes);
 
 export default rootRouter;
